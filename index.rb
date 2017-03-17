33fac8dab68b4fc5284b74e9e544668d6c13327a
lstrzebinczyk <lukasz.strzebinczyk@ac-project.net>
2017-03-17 16:43:53 +0100

    fuck yeah, astar!

--- index.rb
+++ index.rb
@@ -136,14 +136,83 @@ class Path
   end
 end
 
-class Map
-  def passable?(x, y)
-    true
+class Tree
+  def initialize(x, y)
+    @image = Image.new(x * PIXELS_PER_SQUARE, y * PIXELS_PER_SQUARE, "assets/nature/pinetree.png")
+  end
+
+  def rerender
+    @image.remove
+    @image.add
   end
 end
 
-@map = Map.new
+class Grid
+  def initialize
+    @grid_data = {}
+  end
+
+  def []=(x, y, value)
+    @grid_data[x] ||= {}
+    @grid_data[x][y] ||= {}
+    @grid_data[x][y] = value
+  end
+
+  def [](x, y)
+    @grid_data[x] && @grid_data[x][y]
+  end
+
+  def each
+    @grid_data.each do |key, value|
+      value.each do |key, value|
+        yield(value)
+      end
+    end
+  end
+end
+
+class Map
+  def initialize(opts)
+    @width  = opts[:width]
+    @height = opts[:height]
+    @grid   = Grid.new
+
+    fill_grid_with_trees
+  end
+
+  def passable?(x, y)
+    @grid[x, y].nil?
+  end
+
+  def rerender
+    @grid.each do |elem|
+      elem.rerender
+    end
+  end
+
+  private
+
+  def set_tree?
+    rand < 0.20
+  end
+
+  def fill_grid_with_trees
+    (0..@width).each do |x|
+      (0..@height).each do |y|
+        if set_tree?
+          @grid[x, y] = Tree.new(x, y)
+        end
+      end
+    end
+  end
+end
+
+@map = Map.new(width: SQUARES_WIDTH, height: SQUARES_HEIGHT)
 @path = Path.new
+
+def draw_map_things
+  @map.rerender
+end
 
 def calculate_path_to(x, y)
   # puts "Looking path from (#{@character_position.x}, #{@character_position.y}) to  (#{x}, #{y})"
@@ -220,5 +289,6 @@ end
 
 draw_background
 draw_character
+draw_map_things
 
 show
