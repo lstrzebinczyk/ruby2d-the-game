8805fb5cb3dec8e064078fca437170507b4c6c1c
lstrzebinczyk <lukasz.strzebinczyk@ac-project.net>
2017-03-18 02:26:57 +0100

    day and ight cycle

--- lib/index.rb
+++ lib/index.rb
@@ -57,6 +57,46 @@ $character = Character.new(30, 20)
 $fps_drawer = FpsDrawer.new
 $mouse_background_drawer = MouseBackgroundDrawer.new
 
+class DayAndNightCycle
+  def initialize
+    @time = Time.new(1, 1, 1, 12, 0) # start at 12:00 of the first day ever in history
+    @text = Text.new(820, 12, "12:00", 40, "fonts/arial.ttf")
+    @sun_shining_mask = Rectangle.new(0, 0, WIDTH, HEIGHT, sun_mask_color)
+  end
+
+  def time
+    @time.strftime("%H:%M")
+  end
+
+  # Nice blue-ish night sky color
+  def sun_mask_color
+    [0, 33.0 / 255, 115.0 / 255, sun_mask_opacity]
+  end
+
+  # Map in such way that at noon is minimum
+  # At midnight is maximum
+  def sun_mask_opacity
+    time_to_sinus_argument = (@time.hour - 6) * 3.14 / 12.0 + 3.14 / 2
+    [0, Math.cos(time_to_sinus_argument)].max * 0.20
+  end
+
+  # implicitly assume 1 tick means n seconds
+  def update
+    n = 15
+    @time += n
+
+    @text.remove
+    @text.text = time
+    @text.add
+
+    @sun_shining_mask.remove
+    @sun_shining_mask.color = sun_mask_color
+    @sun_shining_mask.add
+  end
+end
+
+$day_and_night_cycle = DayAndNightCycle.new
+
 
 @tick = 0
 def update_with_tick(&block)
@@ -78,6 +118,7 @@ update_with_tick do |tick|
   end
 
   $character.move if tick % 4 == 0
+  $day_and_night_cycle.update
 end
 
 # pre-calculate where passable areas are with flooding the map from characters position
