472565c263e3daae9d3cc41c8e651e4f4b2eb8c0
lstrzebinczyk <lukasz.strzebinczyk@ac-project.net>
2017-04-01 11:09:09 +0200

    make inspect first button

--- lib/gui/menu.rb
+++ lib/gui/menu.rb
@@ -2,11 +2,12 @@ class Menu
   attr_accessor :game_mode
 
   def initialize
-    @game_mode    = CutGameMode.new
     @menu_y_start = WORLD_HEIGHT
     @buttons      = []
 
     render
+
+    @buttons.first.on_click.call
   end
 
   def click(x, y)
@@ -47,8 +48,8 @@ class Menu
   def render
     render_menu_background
 
-    render_button("Cut",           width: 70 )
     render_button("Inspect",       width: 130)
+    render_button("Cut",           width: 70 )
     render_button("Set storage",   width: 220)
     render_button("Build kitchen", width: 240)
   end
