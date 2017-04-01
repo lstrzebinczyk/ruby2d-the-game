004e76e4cba42a8a75b2442a5301f8de744085f1
lstrzebinczyk <lukasz.strzebinczyk@ac-project.net>
2017-04-01 11:10:13 +0200

    fix a bug with kitchen

--- lib/structures/kitchen.rb
+++ lib/structures/kitchen.rb
@@ -28,4 +28,7 @@ class Kitchen < Structure
     @mask = Square.new(x * PIXELS_PER_SQUARE, y * PIXELS_PER_SQUARE, 3 * PIXELS_PER_SQUARE, "brown")
     @mask.color.opacity = 0.6
   end
+
+  def update(time)
+  end
 end
