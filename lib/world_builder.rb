cfa690fd88d5eae82e5cc055eb6124f971089904
lstrzebinczyk <lukasz.strzebinczyk@ac-project.net>
2017-04-02 16:51:08 +0200

    finish test scenario

--- lib/world_builder.rb
+++ lib/world_builder.rb
@@ -46,6 +46,11 @@ class WorldBuilder
   end
 end
 
+class TestScenario
+end
+
+class WoodcuttingTestScenario < TestScenario
+  def initialize
 template = """
 TT.......SS
 TT...B...SS
@@ -53,38 +58,47 @@ TT....W..SS
 TT.......SS
 TT.......SS
 """
-world = WorldBuilder.new(template).build
+    @world = WorldBuilder.new(template).build
 
-cut_game_mode = CutGameMode.new
-$map.each do |elem|
-  if elem.is_a? Tree 
-    cut_game_mode.perform(elem.x, elem.y)
+    cut_game_mode = CutGameMode.new
+    $map.each do |elem|
+      if elem.is_a? Tree 
+        cut_game_mode.perform(elem.x, elem.y)
+      end
+    end
+  end
+
+  def run!
+    iterations = 0
+    while !done?
+      iterations += 1
+      trees_count   = $map.count{|thing| thing.is_a? Tree }
+      @world.update
+    end
+  end
+
+  private
+
+  def no_more_trees?
+    $map.count{|thing| thing.is_a? Tree } == 0
+  end
+
+  def unstored_logs
+    $map.find_all{|thing| thing.is_a? LogsPile }.count do |logs_pile|
+      x = logs_pile.x 
+      y = logs_pile.y
+      !$zones.find{|zone| zone.x == x and zone.y == y}
+    end
+  end
+
+  def all_logs_in_stores?
+    unstored_logs == 0
+  end
+
+  def done?
+    no_more_trees? && all_logs_in_stores?
   end
 end
 
-def no_more_trees?
-  $map.count{|thing| thing.is_a? Tree } == 0
-end
-
-def unstored_logs
-  $map.find_all{|thing| thing.is_a? LogsPile }.count do |logs_pile|
-    x = logs_pile.x 
-    y = logs_pile.y
-    !$zones.find{|zone| zone.x == x and zone.y == y}
-  end
-end
-
-def all_logs_in_stores?
-  unstored_logs == 0
-end
-
-def done?
-  no_more_trees? && all_logs_in_stores?
-end
-
-iterations = 0
-while !done?
-  iterations += 1
-  trees_count   = $map.count{|thing| thing.is_a? Tree }
-  world.update
-end
+test_scenario = WoodcuttingTestScenario.new
+test_scenario.run!
