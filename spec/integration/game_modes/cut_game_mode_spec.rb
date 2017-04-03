52705005eaaae70259072b0dff45590998a9e0d8
lstrzebinczyk <lukasz.strzebinczyk@ac-project.net>
2017-04-03 19:09:54 +0200

    add hover test

--- spec/integration/game_modes/cut_game_mode_spec.rb
+++ spec/integration/game_modes/cut_game_mode_spec.rb
@@ -4,7 +4,7 @@ describe "CutGameMode" do
   it "adds job to cut the tree to jobs list" do
     template = "T"
     @world = WorldBuilder.new(template).build
-    CutGameMode.new.perform(0, 0)
+    CutGameMode.new.click(0, 0)
 
     expect($job_list.count).to eq(1)
     expect($job_list.jobs.first).to be_a (CutTreeJob)
@@ -13,8 +13,8 @@ describe "CutGameMode" do
   it "cancels the job if used for second time" do
     template = "T"
     @world = WorldBuilder.new(template).build
-    CutGameMode.new.perform(0, 0)
-    CutGameMode.new.perform(0, 0)
+    CutGameMode.new.click(0, 0)
+    CutGameMode.new.click(0, 0)
 
     expect($job_list.count).to eq(0)
   end
@@ -22,13 +22,20 @@ describe "CutGameMode" do
   it "doesn't cancel the job if job is taken" do
     template = "T"
     @world = WorldBuilder.new(template).build
-    CutGameMode.new.perform(0, 0)
+    CutGameMode.new.click(0, 0)
 
     $job_list.jobs.first.taken = true
 
-    CutGameMode.new.perform(0, 0)
+    CutGameMode.new.click(0, 0)
 
     expect($job_list.count).to eq(1)
     expect($job_list.jobs.first).to be_a (CutTreeJob)
+  end
+
+  it "hovers and unhovers safely" do
+    template = "."
+    @world = WorldBuilder.new(template).build
+    CutGameMode.new.hover(0, 0)
+    CutGameMode.new.unhover
   end
 end
--- spec/integration/game_modes/remove_game_mode_spec.rb
+++ spec/integration/game_modes/remove_game_mode_spec.rb
@@ -4,7 +4,7 @@ describe "RemoveGameMode" do
   it "adds job to cut the tree to jobs list" do
     template = "B"
     @world = WorldBuilder.new(template).build
-    RemoveGameMode.new.perform(0, 0)
+    RemoveGameMode.new.click(0, 0)
 
     expect($job_list.count).to eq(1)
     expect($job_list.jobs.first).to be_a (CutBerryBushJob)
@@ -13,8 +13,8 @@ describe "RemoveGameMode" do
   it "cancels the job if used for second time" do
     template = "B"
     @world = WorldBuilder.new(template).build
-    RemoveGameMode.new.perform(0, 0)
-    RemoveGameMode.new.perform(0, 0)
+    RemoveGameMode.new.click(0, 0)
+    RemoveGameMode.new.click(0, 0)
 
     expect($job_list.count).to eq(0)
   end
@@ -22,13 +22,20 @@ describe "RemoveGameMode" do
   it "doesn't cancel the job if job is taken" do
     template = "B"
     @world = WorldBuilder.new(template).build
-    RemoveGameMode.new.perform(0, 0)
+    RemoveGameMode.new.click(0, 0)
 
     $job_list.jobs.first.taken = true
 
-    RemoveGameMode.new.perform(0, 0)
+    RemoveGameMode.new.click(0, 0)
 
     expect($job_list.count).to eq(1)
     expect($job_list.jobs.first).to be_a (CutBerryBushJob)
+  end
+
+  it "hovers and unhovers safely" do
+    template = "."
+    @world = WorldBuilder.new(template).build
+    RemoveGameMode.new.hover(0, 0)
+    RemoveGameMode.new.unhover
   end
 end
