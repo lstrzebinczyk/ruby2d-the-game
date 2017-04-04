66ce34331897a5df8c84e76a59b153b49483d638
lstrzebinczyk <lukasz.strzebinczyk@ac-project.net>
2017-04-04 19:05:47 +0200

    make maps less dense

--- lib/utils/random_noise_generator.rb
+++ lib/utils/random_noise_generator.rb
@@ -2,12 +2,12 @@ class RandomNoiseGenerator
   def initialize
     @perlin ||= Perlin::Noise.new(2)
     @contrast ||= Perlin::Curve.contrast(Perlin::Curve::CUBIC, 4)
-    @divider ||= 7.2 + rand / 2
+    @divider ||= 15
   end
 
   def get(x, y)
     n = @perlin[x.to_f / 30, y.to_f / 30]
-    n = @contrast.call(n) / @divider
+    n = @contrast.call(n).to_f / @divider
     n
   end
 end
