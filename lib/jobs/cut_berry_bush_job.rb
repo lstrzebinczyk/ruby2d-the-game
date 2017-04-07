0674aa7a187d80c555ddfc65328dae4482544039
lstrzebinczyk <lukasz.strzebinczyk@ac-project.net>
2017-04-07 21:14:43 +0200

    fix another bug

--- lib/jobs/cut_berry_bush_job.rb
+++ lib/jobs/cut_berry_bush_job.rb
@@ -4,7 +4,7 @@ class CutBerryBushJob
     x = berry_bush.x * PIXELS_PER_SQUARE
     y = berry_bush.y * PIXELS_PER_SQUARE
 
-    @mask = Square.new(@x, @y, PIXELS_PER_SQUARE, [1, 0, 0, 0.2])
+    @mask = Square.new(x, y, PIXELS_PER_SQUARE, [1, 0, 0, 0.2])
   end
 
   def type
