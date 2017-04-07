73a4471bcc7d566b9dd9a85b5520198210e0c5d7
lstrzebinczyk <lukasz.strzebinczyk@ac-project.net>
2017-04-07 21:08:15 +0200

    optimise noise generator

--- lib/utils/random_noise_generator.rb
+++ lib/utils/random_noise_generator.rb
@@ -2,12 +2,12 @@ class RandomNoiseGenerator
   def initialize
     @perlin ||= Perlin::Noise.new(2)
     @contrast ||= Perlin::Curve.contrast(Perlin::Curve::CUBIC, 4)
-    @divider ||= 12
+    @divider ||= 12.0
   end
 
   def get(x, y)
-    n = @perlin[x.to_f / 30, y.to_f / 30]
-    n = @contrast.call(n).to_f / @divider
+    n = @perlin[x * 0.033, y * 0.033]
+    n = @contrast.call(n) / @divider
     n
   end
 end
