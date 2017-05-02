cdfe99ff2dfee13681eddb30d2a35214294c1fb2
lstrzebinczyk <lukasz.strzebinczyk@ac-project.net>
2017-05-02 18:44:30 +0200

    react to map offset on rivers

--- lib/map_elements/river.rb
+++ lib/map_elements/river.rb
@@ -3,7 +3,22 @@ class River
 
   def initialize(x, y)
     @x, @y = x, y
-    @image  = Image.new(x * PIXELS_PER_SQUARE, y * PIXELS_PER_SQUARE, "assets/nature/river.png")
+    @offset_x = $map_position.offset_x
+    @offset_y = $map_position.offset_y
+    @image  = Image.new(
+      @x * PIXELS_PER_SQUARE + @offset_x,
+      @y * PIXELS_PER_SQUARE + @offset_y,
+      "assets/nature/river.png"
+    )
+    $map_position.add_observer(self, :update_offset)
+  end
+
+  def update_offset(offset_x, offset_y)
+    @offset_x = offset_x
+    @offset_y = offset_y
+
+    @image.x = @x * PIXELS_PER_SQUARE + @offset_x
+    @image.y = @y * PIXELS_PER_SQUARE + @offset_y
   end
 
   def remove
