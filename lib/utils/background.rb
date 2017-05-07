891201e0e3454106520a751be427ea67936c8180
lstrzebinczyk <lukasz.strzebinczyk@ac-project.net>
2017-05-07 13:48:55 +0200

    make all background visible

--- lib/utils/background.rb
+++ lib/utils/background.rb
@@ -1,5 +1,7 @@
 class Background
   def initialize
-    @image = MapRenderer.image(0, 0, "assets/nature/background.png", ZIndex::BACKGROUND)
+    MapRenderer.image(0, 0, "assets/nature/background.png", ZIndex::BACKGROUND)
+    MapRenderer.image(0, 790 / PIXELS_PER_SQUARE, "assets/nature/background.png", ZIndex::BACKGROUND)
+    MapRenderer.image(0, 2 * 790 / PIXELS_PER_SQUARE, "assets/nature/background.png", ZIndex::BACKGROUND)
   end
 end
