d14efba5e17a1b8b89b5bb6b4758bd7a897cccb0
lstrzebinczyk <lukasz.strzebinczyk@ac-project.net>
2017-05-12 21:53:22 +0200

    drunkard cave generation

--- games/cave_crawler/index.rb
+++ games/cave_crawler/index.rb
@@ -9,11 +9,11 @@ require "ruby2d"
 
 require "pry"
 
-MAP_WIDTH = 30
-MAP_HEIGHT = 20
+MAP_WIDTH = 60
+MAP_HEIGHT = 40
 
-ENTRY_POINT_X = 18
-ENTRY_POINT_Y = 19
+ENTRY_POINT_X = rand(MAP_WIDTH)
+ENTRY_POINT_Y = MAP_HEIGHT - 1
 
 PIXELS_PER_SQUARE = 12
 
@@ -25,9 +25,12 @@ set({
 })
 
 class MapPoint
-  def initialize(x, y)
+  attr_reader :filled
+
+  def initialize(x, y, parent)
     @x, @y = x, y
     @filled = true
+    @parent = parent
     @mask = Square.new(
       @x * PIXELS_PER_SQUARE,
       @y * PIXELS_PER_SQUARE,
@@ -37,8 +40,29 @@ class MapPoint
   end
 
   def empty
-    @filled = true
+    @filled = false
     @mask.color = "gray"
+  end
+
+  def activate!
+    @mask.color = "red"
+  end
+
+  def deactivate!
+    if @filled
+      @mask.color = "brown"
+    else
+      @mask.color = "gray"
+    end
+  end
+
+  def random_neighbor
+    [
+      @parent[@x-1, @y],
+      @parent[@x+1, @y],
+      @parent[@x, @y+1],
+      @parent[@x, @y-1]
+    ].compact.sample
   end
 end
 
@@ -46,12 +70,32 @@ $grid = Grid.new
 
 MAP_WIDTH.times do |x|
   MAP_HEIGHT.times do |y|
-    $grid[x, y] = MapPoint.new(x, y)
+    $grid[x, y] = MapPoint.new(x, y, $grid)
   end
 end
 
 # binding.pry
 
-$grid[ENTRY_POINT_X, ENTRY_POINT_Y].empty
+active_map_point = $grid[ENTRY_POINT_X, ENTRY_POINT_Y]
+
+active_map_point.activate!
+active_map_point.empty
+
+GENERATE_FLOORS = (MAP_WIDTH * MAP_HEIGHT * 0.3).to_i
+
+$generated_floors = 1
+
+update do
+  if $generated_floors < GENERATE_FLOORS
+    active_map_point.deactivate!
+    active_map_point = active_map_point.random_neighbor
+    active_map_point.activate!
+    if active_map_point.filled
+      active_map_point.empty
+      $generated_floors += 1
+    end
+  end
+end
+
 
 show
